c0a576688bdfc40b83a9b771902a572a2eeb9d04
Cauteros974 <pusik6666@ukr.net>
2025-09-28 19:08:47 +0300

    commit

--- src/assets/pages/CatalogPage.tsx
+++ src/assets/pages/CatalogPage.tsx
@@ -15,7 +15,7 @@ const ProductCard = ({ product }: { product: Product }) => {
       className="border rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col"
     >
       <Link to={`/product/${product.id}`} className="flex-grow">
-        <img src={product.imageUrl} alt={product.name} className="w-full h-56 object-cover" />
+        <img src={product.image} alt={product.name} className="w-full h-56 object-cover" />
         <div className="p-4">
           <h3 className="text-lg font-semibold truncate">{product.name}</h3>
           <p className="text-gray-500">{product.category}</p>

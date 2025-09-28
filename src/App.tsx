c9f1c34022453fe034720734e013709cf8e4885b
Cauteros974 <pusik6666@ukr.net>
2025-09-28 18:11:14 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ function App() {
         <Header />
         <main className="flex-grow container mx-auto px-4 py-8">
           <Routes>
-            <Route path="/" element={<HomePage />} />
+          
             <Route path="/catalog" element={<CatalogPage />} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
+            
             <Route path="/cart" element={<CartPage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
           </Routes>

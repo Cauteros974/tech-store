b6317c3501c52e56ae168eaf60ea804be5e8dcab
Cauteros974 <pusik6666@ukr.net>
2025-09-28 17:49:01 +0300

    Update CheckoutPage.tsx

--- src/assets/pages/CheckoutPage.tsx
+++ src/assets/pages/CheckoutPage.tsx
@@ -15,6 +15,28 @@ const CheckoutPage = () => {
     const navigate = useNavigate();
 
     const totalPrice = items.reduce((total, item) => total + item.price * item.quantity, 0);
+
+    const onSubmit: SubmitHandler<FormValues> = ( data ) {
+        if(items.length === 0) {
+            alert("Your cart is empty");
+            navigate('/catalog');
+            return;
+        };
+
+        const order  = {
+            customer: data,
+            items: items, 
+            total: totalPrice,
+            orderDate: new Date().toISOString(),
+        };
+
+        const pastOrders = JSON.parse(localStorage.getItem('orders') || '[]');
+        localStorage.setItem('orders', JSON.stringify([...pastOrders, order]));
+        
+        alert('Your order has been successfully placed!');
+        clearCart()
+        navigate('/');
+    };
 };
 
 export default CheckoutPage;

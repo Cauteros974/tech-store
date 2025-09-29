531df5affe3f1bd60fee10dc738edc47584ab328
Cauteros974 <pusik6666@ukr.net>
2025-09-29 21:55:13 +0300

    Create ProtectedRoute.tsx

---
+++ src/components/shared/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../../store/authStore";
+
+const ProtectedRoute = () => {
+    const { user, loading } = useAuthStore();
+
+    if (loading) {
+        return <div className="flex justify-center items-center h-screen">Loading...</div>
+    }
+};
+
+export default ProtectedRoute;

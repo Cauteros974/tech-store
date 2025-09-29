8032894b03238a05494feb9d24e61c7ae8709a11
Cauteros974 <pusik6666@ukr.net>
2025-09-29 20:56:10 +0300

    commit

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,16 +1,17 @@
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer"
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 const Layout = () => {
-    return(
-        <div className="flex flex-col min-h-screen bg-gray-50 font-sans text-gray-800">
-            <Header />
-            <main className="flex-grow container mx-auto px-4 py-8 md:py-12">
-                <Outlet />
-            </main>
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50 font-sans text-gray-800">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8 md:py-12">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;

739599f044b710f19caad41a7c82019f9af6b8ab
Cauteros974 <pusik6666@ukr.net>
2025-09-30 20:44:53 +0300

    Update CategoryPage.tsx

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -2,3 +2,13 @@ import { useEffect, useState, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Product } from "../store/cartStore";
+import { ProductCard } from "../components/products/ProductCard";
+import { motion } from "framer-motion";
+
+const CategoryPage = () => {
+    const { categoryName } = useParams <{ categoryName: string }>();
+    const [ products, setProducts ] = useState<Product[]>([]);
+    const [ loading, setLoading] = useState(true);
+};
+
+export default  CategoryPage;

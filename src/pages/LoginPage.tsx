7c741c13826a79f1865d8c72bd23d2bae69d412c
Cauteros974 <pusik6666@ukr.net>
2025-10-01 18:34:19 +0300

    Create LoginPage.tsx

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { useForm } from "react-hook-form";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { Navigate, useNavigate } from "react-router-dom";
+
+const handleLogin = async ( data ) => {
+    try {
+        await signInWithEmailAndPassword(auth, data.email, data.password);
+        navigate('/');
+    }
+}
